50dc287a9fc0d6d0fa74333d858a03744059a0f2
KindPo4atoc <nicita.haleev@yandex.ru>
2025-04-11 03:05:06 +0300

    Добавлена страница demo с реализацией api

---
+++ js/ldaRequest.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    const requestAllDataLearn = "http://localhost:8000/selectLearnData"
+    const requestTestData = "http://localhost:8000/selectTestData"
+    const requestInitLDA = "http://localhost:8000/initData"
+
+}
